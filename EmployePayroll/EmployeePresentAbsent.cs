bd6755588c4af7bb1d4cd57ce660e69638f9efe4
Komal <waghmarekomal21@gmail.com>
2022-08-28 00:07:49 +0530

    [Komal]EmployeeAttendace

---
+++ EmployePayroll/EmployeePresentAbsent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployePayroll
+{
+    internal class EmployeePresentAbsent
+    {
+        public static void Random()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+
+            switch (empcheck = 0)
+            {
+                case 0:
+                    Console.WriteLine("is present");
+                    break;
+                case 1:
+                    Console.WriteLine("is abscent");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
