de4096ccfeceb17a07a0bc07e6d799348b12898e
Komal <waghmarekomal21@gmail.com>
2022-08-29 01:01:59 +0530

    [Komal]Welcome Message updated

--- EmployePayroll/Program.cs
+++ EmployePayroll/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome To Employee Wage!");
+            Console.WriteLine("Choose option\n 1.Check absent or present\n2.Check daily employee wage\n3.Part time wage\n4.Part time wage using switch case\n5.Monthly Wages\n6.Total Working Hours Of Employe");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    EmployeePresentAbsent.Random();
+                    break;
+                case 2:
+                    Dailywages.CalculateDailyWage();
+                    break;
+                case 3:
+                    PartTimeWages.CalculatePercentage();
+                    break;
+                case 4:
+                    PartTimeWagesSwitchcase.CalculateWage();
+                    break;
+                case 5:
+                    MonthlyWage.CalculateMonthlyWage();
+                    break;
+                case 6:
+                    TotalWorkingHours.WorkingHours();
+                    break;
+                default:
+                    Console.WriteLine("Entered wrong choice");
+                    break;
+            }
+            
         }
     }
 }
+              
